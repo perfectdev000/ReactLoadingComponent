14536cb4193502a47439d150c99e5680f1851921
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-03-10 22:34:47 +0900

    added loading1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import '../styles/animation.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Loading1 from './components/Loading1'
 
 const Home: NextPage = () => {
   return (
@@ -13,58 +14,32 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org" >Next.js!</a>
+        <h1 className="text-[40px]">
+          Loading Components
         </h1>
+        <div className='flex flex-wrap justify-center items-center w-full mt-8'>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
+            <Loading1/>
+          </div>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+          </div>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p className='text-40 font-medium bg-gray-300'>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </div>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          </div>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          </div>
+          <div className='w-60 h-60 border-2 rounded-xl m-16'>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
+
+
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
